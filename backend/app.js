91716038be2ce0cd8c3c9ce0edd6568e09f09b1f
Iren Galygina <galigina01@yandex.ru>
2022-08-24 21:51:41 +0300

    Первая итерация ПР15

--- backend/app.js
+++ backend/app.js
@@ -72,10 +72,6 @@ app.use((err, _req, res, next) => {
   const { statusCode = 500, message } = err;
   res
     .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка'
-        : message,
-    });
+    .send({ message });
   next();
 });
